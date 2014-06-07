7dfe45d2955e6de78e6373c2230d9c3cde969bb7
Touchebag <someone90_6@hotmail.com>
2014-06-07 18:43:10 +0200

    Added comments

--- src/deck.cpp
+++ src/deck.cpp
@@ -1,31 +1,42 @@
 #include "deck.h"
 
+// Adds two of the same card with less delay
 void addTwoCards(std::string card){
   addCard(card);
   Sleep(200);
   clickMouse();
 }
 
+// Adds a card to the deck
 void addCard(std::string card){
+  // Move mouse to search window
   moveMouse(0.5,0.92);
   clickMouse();
-
   Sleep(500);
+
+  // Input card name
   typeString(card);
   Sleep(500);
+
+  // Add card to deck
   moveMouse(0.2,0.3);
   clickMouse();
 }
 
+// Reads a deck from a file
 void readDeck(std::string file){
+  // Open file
   std::ifstream infile;
   infile.open(file.c_str());
 
+  // The current card
   std::string card;
+
   while (std::getline(infile, card)){
     addCard(card);
     Sleep(500);
   }
 
+  // Close file
   infile.close();
 }
--- src/deck.h
+++ src/deck.h
@@ -8,9 +8,12 @@
 #include "mouse.h"
 #include "keyboard.h"
 
+// Adds two of the same card with less delay
 void addCard(std::string);
+// Adds a card to the deck
 void addTwoCards(std::string);
 
+// Reads a deck from a file
 void readDeck(std::string);
 
 #endif
