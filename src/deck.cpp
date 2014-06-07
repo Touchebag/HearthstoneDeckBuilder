09c514d266581288456545b7d189362f8b23b032
Touchebag <someone90_6@hotmail.com>
2014-06-07 17:29:07 +0200

    Can now read from file

--- src/deck.cpp
+++ src/deck.cpp
@@ -1,4 +1,30 @@
 #include "deck.h"
 
-void addCard(std::string){
+void addTwoCards(std::string card){
+  addCard(card);
+  Sleep(200);
+  clickMouse();
+}
+
+void addCard(std::string card){
+  moveMouse(0.5,0.92);
+  clickMouse();
+
+  Sleep(500);
+  typeString(card);
+  Sleep(500);
+  moveMouse(0.2,0.3);
+  clickMouse();
+}
+
+void readDeck(std::string file){
+  std::ifstream infile;
+  infile.open(file.c_str());
+
+  std::string card;
+  while (std::getline(infile, card)){
+    std::cout << card;
+  }
+
+  infile.close();
 }
--- src/deck.h
+++ src/deck.h
@@ -2,6 +2,15 @@
 #define Deck_h
 
 #include <iostream>
+#include <fstream>
+#include <sstream>
+
+#include "mouse.h"
+#include "keyboard.h"
 
 void addCard(std::string);
+void addTwoCards(std::string);
+
+void readDeck(std::string);
+
 #endif
