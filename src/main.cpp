68dd7903d3f5315c1632a31358dc2ab90aaeef39
Touchebag <someone90_6@hotmail.com>
2014-07-16 17:06:44 +0200

    Tidied up the main menu

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,8 @@
 #include "../include/deck.h"
 
 int main(int argc, char *argv[]){
+  system("cls");
+
   while (true){
     std::cout << "[0] Exit"  << std::endl <<
       "[1] Create a deck"    << std::endl <<
@@ -15,15 +17,20 @@ int main(int argc, char *argv[]){
       "Choose an option: ";
     int option;
     std::cin >> option;
+    std::string file;
 
     switch (option){
       case 1:
+        // Allow for specifing a custom deck file
+        std::cout << "Please specify a deck file. If no file is specified the default (deck.txt) will be read." << std::endl;
+        std::cin >> file;
+
         // Pause for 5 seconds.
+        std::cout << "Preparing to create deck from file \"" << file <<"\". Please switch to the Hearthstone window." << std::endl;
         Sleep(5000);
 
-        // Allow for specifing a custom deck file
-        if (argc > 1)
-          readDeck(argv[1]);
+        if (file.empty())
+          readDeck(file);
         else
           readDeck("deck.txt");
         break;
@@ -32,11 +39,13 @@ int main(int argc, char *argv[]){
         return 0;
         break;
       case 2:
-        std::cout << "TBI";
+        std::cout << "TBI" << std::endl;
         break;
       default:
-        std::cout << "Unrecognised option";
+        std::cout << "Unrecognised option" << std::endl;
         break;
     }
+
+    system("pause");
   }
 }
